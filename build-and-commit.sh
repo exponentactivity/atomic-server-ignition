0e2f80901e0b6d39970add0c8d41e00631737bf5
exponentactivity <18619090+exponentactivity@users.noreply.github.com>
2025-03-10 19:12:02 +0100

    Change default username and add build script

---
+++ build-and-commit.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+podman run --interactive --rm --security-opt label=disable \
+       --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release \
+       --pretty --strict base.bu > base.ign
+
+podman run --interactive --rm --security-opt label=disable \
+       --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release \
+       --pretty --strict nas0.bu > nas0.ign
+
+podman run --interactive --rm --security-opt label=disable \
+       --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release \
+       --pretty --strict serv0.bu > serv0.ign
+
+podman run --interactive --rm --security-opt label=disable \
+       --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release \
+       --pretty --strict modules/image-base.bu > modules/image-base.ign
+
+podman run --interactive --rm --security-opt label=disable \
+       --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release \
+       --pretty --strict modules/image-nas0.bu > modules/image-nas0.ign
+
+podman run --interactive --rm --security-opt label=disable \
+       --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release \
+       --pretty --strict modules/image-serv0.bu > modules/image-serv0.ign
+
+podman run --interactive --rm --security-opt label=disable \
+       --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release \
+       --pretty --strict modules/user-default-unpriv.bu > modules/user-default-unpriv.ign
+
+podman run --interactive --rm --security-opt label=disable \
+       --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release \
+       --pretty --strict modules/user-containers.bu > modules/user-containers.ign
+
+git add .
+git commit
